6afdc4c414fce41ebb0d8d675ac4649bdde55c64
Nick <95316598+NickRTR@users.noreply.github.com>
2023-08-30 02:19:46 +0200

    Navigation Style improvements

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,34 +1,60 @@
 <nav>
-	<a href="/host">Host</a>
+	<div class="links">
+		<a href="/host">Host</a>
+		<a href="/host">Calendar</a>
+	</div>
 	<a href="/" class="codathon">Codathon</a>
-	<a href="/account"><img src="/user.svg" alt="Account" /></a>
+	<a class="account" href="/account"><img src="/profile.jpg" alt="👤" /></a>
 </nav>
 
 <style>
 	nav {
 		background-color: var(--text);
-		display: flex;
-		justify-content: space-between;
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
 		align-items: center;
-		padding: 1rem;
+		padding: 0.5rem 1rem;
 	}
 
 	a {
 		color: var(--background);
 		text-decoration: none;
 		font-family: "Lemon";
+		outline: none;
+	}
+
+	a:hover,
+	a:focus {
+		color: var(--accent);
+	}
+
+	.links {
+		text-align: left;
+		display: flex;
+		gap: 0.5rem;
 	}
 
 	.codathon {
 		font-size: 1.5rem;
 		font-weight: bold;
+		text-align: center;
 	}
 
-	a:hover {
-		color: var(--accent);
+	.account {
+		text-align: right;
+		color: aqua;
 	}
 
 	img {
-		height: 1.5rem;
+		height: 2.5rem;
+		border-radius: 100%;
+		margin-top: 0.3rem;
+		border: 2px solid var(--text);
+	}
+
+	img:hover,
+	img:focus,
+	a:focus img {
+		border-color: var(--accent);
 	}
 </style>
